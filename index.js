173956a9b4321de7b26363f834f0636cd50da263
Delphine30 <kamusabwadelphine@gmail.com>
2023-03-28 14:25:05 +0200

    learning

---
+++ index.js
@@ -0,0 +1,13 @@
+var student="female";
+var skills="basics";
+var text=80;
+//we are going to allocate student according to their performance  and gender
+if (student=="female" && skill=="none") {
+    console.log("your class is shecancode beginner");
+}else if (student=="female"&& skills=="basics"){
+    console.log("your class is shecancode advanced ");
+}else{
+    console.log("your class is teck-code");
+}
+
+
